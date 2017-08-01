ebec1953d96e4e8c96c6b34cd1c44616c136a52d
David RJ <david.rud135@gmail.com>
2017-08-01 17:41:04 +0300

    Updated code

--- script.js
+++ script.js
@@ -1,61 +1,44 @@
-function calculate() {
-	var leg1, leg2, hypo, firstCondition, secondCondition, thirdCondition;
-	leg1 = parseInt(document.getElementById('leg1').value.trim() );
-	leg2 = parseInt(document.getElementById('leg2').value.trim() );
-	hypo = parseInt(document.getElementById('hypotenuse').value.trim() ); 
+function calculateThirdSide() {
+	var leg1, leg2, hypo;
+	leg1 = getFieldDataById('leg1');
+	leg2 = getFieldDataById('leg2');
+	hypo = getFieldDataById('hypotenuse');
 
-	firstCondition = isNaN(leg1) && isNaN(leg2);
-	secondCondition = isNaN(hypo) && isNaN(leg1);
-	thirdCondition = isNaN(hypo) && isNaN(leg2);
+	checkForTwoEmptyFields(leg1, leg2, hypo); 
 
-	if (firstCondition || secondCondition  || thirdCondition) {
-		alert('At least 2 fiels should be filled up!');
-	} else if ( isNaN(leg1) ) {
-
-		findLeg1(leg2, hypo);
-		highlightResultLeg1();
-
-	} else if ( isNaN(leg2) ) {
-
-		findLeg2(leg1, hypo);
-		highlightResultLeg2();
-
-	} else if ( isNaN(hypo) ) {
-
-		findHypo(leg1, leg2);
-		highlightResultHypo();
-
-	} else {
-		alert('Error!\nPlease try again');
+	switch(true) {
+		case isItNaN(leg1):
+			findLeg1(leg2, hypo);
+			highlightResultById('leg1');
+			break;
+		case isItNaN(leg2):
+			findLeg2(leg1, hypo);
+			highlightResultById('leg2');
+			break;
+		case isItNaN(hypo):
+			findHypo(leg1, leg2);
+			highlightResultById('hypotenuse');
+			break;
 	}
 }
 
-function clearFields() {
-	var firstFieldValue, secondFieldValue, thirdFieldValue;
-	var field1, field2, field3;
+function clearAllFields() {
+	var resultField = document.querySelector('.highlighted_result');
+	resultField.className = 'field';
 
-	firstFieldValue = document.getElementById('leg1').value = '';
-	secondFieldValue = document.getElementById('leg2').value = '';
-	thirdFieldValue = document.getElementById('hypotenuse').value = '';	
-
-	field1 = document.getElementById('leg1');
-	field2 = document.getElementById('leg2');
-	field3 = document.getElementById('hypotenuse');
-
-	field1.style.fontWeight = 'normal';
-	field2.style.fontWeight = 'normal';
-	field3.style.fontWeight = 'normal';
-
-	field1.style.color = '#000';
-	field2.style.color = '#000';
-	field3.style.color = '#000';
+	clearFieldDataById('leg1');
+	clearFieldDataById('leg2');
+	clearFieldDataById('hypotenuse');
 }
 
-function findHypo(leg1, leg2) {
-	leg1 = toSquare(leg1);
-	leg2 = toSquare(leg2);
-	hypo = Math.sqrt(leg1 + leg2);
-	return document.getElementById('hypotenuse').value = hypo.toFixed(3);
+function checkForTwoEmptyFields(leg1, leg2, hypo) {
+	switch(true) {
+		case isItNaN(leg1) && isItNaN(leg2):
+		case isItNaN(leg1) && isItNaN(hypo):
+		case isItNaN(leg2) && isItNaN(hypo):
+			alert('At least two fields should be filled up!');
+			break;
+	}
 }
 
 function findLeg1(leg2, hypo) {
@@ -65,7 +48,7 @@ function findLeg1(leg2, hypo) {
 		leg2 = toSquare(leg2);
 		hypo = toSquare(hypo);
 		leg1 = Math.sqrt(hypo - leg2);
-		return document.getElementById('leg1').value = leg1.toFixed(3);
+		document.getElementById('leg1').value = leg1.toFixed(2);
 	}
 }
 
@@ -76,26 +59,32 @@ function findLeg2(leg1, hypo) {
 		leg1 = toSquare(leg1);
 		hypo = toSquare(hypo);
 		leg2 = Math.sqrt(hypo - leg1);
-		return document.getElementById('leg2').value = leg2.toFixed(3);
+		document.getElementById('leg2').value = leg2.toFixed(2);
 	}
 }
 
-function highlightResultHypo() {
-	var resultInput = document.getElementById('hypotenuse');
-	resultInput.style.color = 'blue';
-	resultInput.style.fontWeight = 'bold';
+function findHypo(leg1, leg2) {
+	leg1 = toSquare(leg1);
+	leg2 = toSquare(leg2);
+	hypo = Math.sqrt(leg1 + leg2);
+	document.getElementById('hypotenuse').value = hypo.toFixed(2);
 }
 
-function highlightResultLeg1() {
-	var resultInput = document.getElementById('leg1');
-	resultInput.style.color = 'blue';
-	resultInput.style.fontWeight = 'bold';
+function highlightResultById(id) {
+	var resultField = document.getElementById(id);
+	resultField.classList.add('highlighted_result');
 }
 
-function highlightResultLeg2() {
-	var resultInput = document.getElementById('leg2');
-	resultInput.style.color = 'blue';
-	resultInput.style.fontWeight = 'bold';
+function clearFieldDataById(id) {
+	document.getElementById(id).value = '';
+}
+
+function getFieldDataById(id) {
+	return parseInt(document.getElementById(id).value.trim() );
+}
+
+function isItNaN(item) {
+	return isNaN(item);
 }
 
 function toSquare(x) {
