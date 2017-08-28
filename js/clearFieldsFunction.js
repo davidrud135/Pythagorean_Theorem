27feabf0084013a99e11bcb8c71174ea8380f4d4
David RJ <david.rud135@gmail.com>
2017-08-28 17:30:09 +0300

    Deleted

--- js/clearFieldsFunction.js
+++
@@ -1,7 +0,0 @@
-function clearAllFields() {
-    $('.highlighted_result').removeClass('highlighted_result');
-
-    clearFieldDataById('leg1');
-    clearFieldDataById('leg2');
-    clearFieldDataById('hypotenuse');
-}
