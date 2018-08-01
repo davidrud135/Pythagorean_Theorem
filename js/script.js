18558b0b998842a79903caec0dda025446eba9de
David R.J <david.rud135@gmail.com>
2018-08-01 16:07:11 +0300

    Just a few minor fixes

--- js/script.js
+++ js/script.js
@@ -1,58 +1,50 @@
 $(document).ready(function() {
-
     $('form').submit(function calculateThirdSide() {
-        var leg1, leg2, hypo;
+        let leg1, leg2, hypo;
         leg1 = getFieldDataById('leg1');
         leg2 = getFieldDataById('leg2');
         hypo = getFieldDataById('hypotenuse'); 
 
-        if ( checkForTwoEmptyFields(leg1, leg2, hypo) ) { 
+        if (checkForTwoEmptyFields(leg1, leg2, hypo)) { 
             return alert('At least two fields should be filled up!');
         }
         
-        if ( isNaN(leg1) ) {
-
+        if (isNaN(leg1)) {
             leg1 = findLeg(leg2, hypo);
             bringOutTheResult('leg1', leg1);
             highlightResultById('leg1');
-
-        } else if ( isNaN(leg2) ) {
-
+        } else if (isNaN(leg2)) {
             leg2 = findLeg(leg1, hypo);
             bringOutTheResult('leg2', leg2);
             highlightResultById('leg2');
-
-        } else if ( isNaN(hypo) ) {
-
+        } else if (isNaN(hypo)) {
             hypo = findHypo(leg1, leg2);
             bringOutTheResult('hypotenuse', hypo);
             highlightResultById('hypotenuse');
-            
-        }    
-
+        }
         return false;
     });
 
     $('input[type=button]').click(function clearAllFields() {
         $('.highlighted_result').removeClass('highlighted_result');
-
         clearFieldDataById('leg1');
         clearFieldDataById('leg2');
         clearFieldDataById('hypotenuse');
     });
 
     function getFieldDataById(id) {
-        return parseInt($('#' + id).val().trim() );
+        return parseInt( $('#' + id).val().trim() );
     }
 
     function checkForTwoEmptyFields(leg1, leg2, hypo) {
-        var firstAndSecondFieldsIsEmpty = isNaN(leg1) && isNaN(leg2);
-        var firstAndThirdFieldsIsEmpty = isNaN(leg1) && isNaN(hypo);
-        var secondAndThirdFieldsIsEmpty = isNaN(leg2) && isNaN(hypo); 
+        const firstAndSecondFieldsAreEmpty = isNaN(leg1) && isNaN(leg2);
+        const firstAndThirdFieldsAreEmpty = isNaN(leg1) && isNaN(hypo);
+        const secondAndThirdFieldsAreEmpty = isNaN(leg2) && isNaN(hypo); 
          
-        if (firstAndSecondFieldsIsEmpty || firstAndThirdFieldsIsEmpty || secondAndThirdFieldsIsEmpty) {
+        if (firstAndSecondFieldsAreEmpty || firstAndThirdFieldsAreEmpty || secondAndThirdFieldsAreEmpty) {
             return true;
         }
+        return false;
     }
 
     function findLeg(leg, hypo) {
@@ -61,7 +53,7 @@ $(document).ready(function() {
         } else {
             leg = toSquare(leg);
             hypo = toSquare(hypo);
-            var leg2 = Math.sqrt(hypo - leg);
+            let leg2 = Math.sqrt(hypo - leg);
             return leg2;
         }
     }
@@ -74,13 +66,11 @@ $(document).ready(function() {
     }
 
     function bringOutTheResult(fieldId, result) {
-        $('#' + fieldId).val(function() {
-            return result.toFixed(2);
-        });
+        $(`#${fieldId}`).val(() => result.toFixed(2));
     }
 
     function highlightResultById(id) {
-        $('#' + id).addClass('highlighted_result');
+        $(`#${id}`).addClass('highlighted_result');
     }
 
     function toSquare(x) {
@@ -88,8 +78,6 @@ $(document).ready(function() {
     }
 
     function clearFieldDataById(id) {
-        $('#' + id).val(function() {
-            return '';
-        });
+        $(`#${id}`).val(() => '');
     }
 });
